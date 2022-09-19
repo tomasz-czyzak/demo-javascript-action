892b025b99ca037e6817c02f7d93d41570d5dbca
Tomasz Czyżak <tomasz.czyzak@gmail.com>
2022-09-19 21:10:42 +0200

    combined

--- verify/src/index.js
+++ verify/src/index.js
@@ -2,30 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   console.log(`***********************`);
+  const prTitle = JSON.stringify(github.context.payload.pull_request.title, undefined, 2)
+  const prBody = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
+  const base = github.context.payload.pull_request?.base?.sha
+  const head = github.context.payload.pull_request?.head?.sha
+  const nameToGreet = core.getInput('modifed_files');
+
+  console.log(`PR SHA BASE:${base}`);
+  console.log(`PR SHA HEAD:${head}`);
+
+  console.log(`PR TITLE:${prTitle}`);
+  console.log(`PR BODY:${prBody}`);
+  console.log(`modifed_files: ${modifed_files}!`);
+  console.log(`***********************`);
+    
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   console.log("***********************");
     
-  const prTitle = JSON.stringify(github.context.payload.pull_request.title, undefined, 2)
-  const prBody = JSON.stringify(github.context.payload.pull_request.body, undefined, 2)
-    
-  const base = github.context.payload.pull_request?.base?.sha
-  const head = github.context.payload.pull_request?.head?.sha
-     
-  console.log(`PR SHA BASE:${base}`);
-  console.log(`PR SHA HEAD:${head}`);
-
-
-  console.log(`PR TITLE:${prTitle}`);
-  console.log(`PR BODY:${prBody}`);
-  console.log(`***********************`);
 } catch (error) {
   core.setFailed(error.message);
 }
