f717394d2817284de2c3a98d8bab5eba916ddffe
Tomasz Czyżak <tomasz.czyzak@gmail.com>
2022-09-19 13:13:28 +0200

    Fix: update log

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
+  console.log("***********************");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log("***********************");
 } catch (error) {
   core.setFailed(error.message);
 }
